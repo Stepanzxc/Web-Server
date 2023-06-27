6d72522414a37830f039905f0000cba26c37e349
Stepan <stepan@192.168.0.19>
2023-06-27 22:37:25 +0300

    make work

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Prod struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Brand       string `json:"brand"`
+	Category    string `json:"category"`
+}
+
+var Products []Prod
+
+func storeDataInMemory(filename string) {
+	//TODO::прочитать файл продуктcsv  в JSON структуре и выгрузить в память приложения, зарабатает до запуска сервера
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var rec Prod
+			for j, field := range line {
+				if j == 0 {
+					rec.Id, _ = strconv.Atoi(field)
+				} else if j == 1 {
+					rec.Title = field
+				} else if j == 2 {
+					rec.Description = field
+				} else if j == 3 {
+					rec.Price, _ = strconv.Atoi(field)
+				} else if j == 4 {
+					rec.Brand = field
+				} else if j == 5 {
+					rec.Category = field
+				}
+
+			}
+			Products = append(Products, rec)
+		}
+	}
+	defer Time(time.Now(), "storeDataInMemory")
+}
+func main() {
+	storeDataInMemory("products.csv")
+	http.HandleFunc("/products", GetProducts)
+	http.HandleFunc("/products/file", GetProductsFromFile)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+func GetProductsFromFile(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("products.csv")
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	var prod []Prod
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var rec Prod
+			for j, field := range line {
+				if j == 0 {
+					rec.Id, _ = strconv.Atoi(field)
+				} else if j == 1 {
+					rec.Title = field
+				} else if j == 2 {
+					rec.Description = field
+				} else if j == 3 {
+					rec.Price, _ = strconv.Atoi(field)
+				} else if j == 4 {
+					rec.Brand = field
+				} else if j == 5 {
+					rec.Category = field
+				}
+
+			}
+			prod = append(prod, rec)
+		}
+	}
+	a, err := json.Marshal(prod)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Write(a)
+	defer Time(time.Now(), "GetProductsFromFile")
+}
+func Time(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+// *http.Request - информация о запросе от клиента
+// http.ResponseWriter - что сервер ответит клиенту
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+
+	//TODO::нужно вывести на сервер файл в JSON формате продуктыcsv
+	a, err := json.Marshal(Products)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Write(a)
+}
