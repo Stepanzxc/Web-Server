948586ef4ea2122bc54a474f3505e4d3a447b21f
Stepan <stepan@192.168.0.19>
2023-07-02 00:52:16 +0300

    update work1

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -23,20 +22,7 @@ type Error struct {
 }
 
 var Products []Prod
-var errr error
 
-func ErrorFun(w http.ResponseWriter) {
-	var errs Error = Error{errr.Error()}
-	log.Println(errs)
-	a, err := json.Marshal(errs)
-	if err != nil {
-		log.Println(err)
-	}
-	_, err = w.Write(a)
-	if err != nil {
-		log.Println(err)
-	}
-}
 func storeDataInMemory(filename string) error {
 	//TODO::прочитать файл продуктcsv  в JSON структуре и выгрузить в память приложения, зарабатает до запуска сервера
 	file, err := os.Open(filename)
@@ -81,67 +67,59 @@ func storeDataInMemory(filename string) error {
 
 	return err
 }
-func GetSomeProduct(w http.ResponseWriter, r *http.Request, n int) {
-	if errr == nil {
-		for i := range Products {
-			if Products[i].Id == n {
-				n = i
-			}
-		}
-		a, err := json.Marshal(Products[n])
-		if err != nil {
-			log.Println(err)
-			Responce(w, a)
-		}
-		Responce(w, a)
-	} else {
-		ErrorFun(w)
+func ErrorFun(w http.ResponseWriter, err error) {
+	var e Error = Error{err.Error()}
+	a, err := json.Marshal(e)
+	if err != nil {
+		log.Println(err)
 	}
+	_, err = w.Write(a)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func GetSomeProduct(w http.ResponseWriter, r *http.Request, n int) {
+	for i := range Products {
+		if Products[i].Id == n {
+			n = i
+		}
+	}
+	a, err := json.Marshal(Products[n])
+	if err != nil {
+		log.Println(err)
+		ErrorFun(w, err)
+		return
+	}
+	Responce(w, a)
+
 }
 func Responce(w http.ResponseWriter, a []byte) {
-	if errr == nil {
-		_, err := w.Write(a)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		ErrorFun(w)
+	_, err := w.Write(a)
+	if err != nil {
+		log.Println(err)
+		ErrorFun(w, err)
+		return
 	}
+
 }
 func main() {
-	errr = storeDataInMemory("products.csv")
-	if errr != nil {
-		log.Println(errr)
+	err := storeDataInMemory("products.csv")
+	if err != nil {
+		log.Fatal(err)
 	}
-	//Нельзя трогать переменную errr
-	// "⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-	// "⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆",
-	// "⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿",
-	// "⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀",
-	// "⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀",
-	// "⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-	// "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉			   "
-	errr = errors.New("Shrek Zzzzz...!")
-
 	http.HandleFunc("/products/3", GetProduct3)
 	http.HandleFunc("/products/14", GetProduct14)
 	http.HandleFunc("/products/17", GetProduct17)
 	http.HandleFunc("/products/30", GetProduct30)
 	http.HandleFunc("/products", GetProducts)
-	errr = http.ListenAndServe(":8080", nil)
-	if errr != nil {
-		log.Println(errr)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println(err)
 	}
 }
+
 func GetProduct17(w http.ResponseWriter, r *http.Request) {
+
 	GetSomeProduct(w, r, 17)
 }
 func GetProduct30(w http.ResponseWriter, r *http.Request) {
@@ -162,7 +140,8 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 	//TODO::нужно вывести на сервер файл в JSON формате продуктыcsv
 	a, err := json.Marshal(Products)
 	if err != nil {
-		log.Println(err)
+		ErrorFun(w, err)
+		return
 	}
 	Responce(w, a)
 }
