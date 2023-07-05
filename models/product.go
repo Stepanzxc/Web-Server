bd88ac25be5f6d226c89529ad164ec574fe0e2cd
behran <agolovev@gismeteo.ru>
2023-07-05 20:07:44 +0300

    :zap: refactor code && create packages

---
+++ models/product.go
@@ -0,0 +1,12 @@
+package models
+
+var Products []Prod
+
+type Prod struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Brand       string `json:"brand"`
+	Category    string `json:"category"`
+}
