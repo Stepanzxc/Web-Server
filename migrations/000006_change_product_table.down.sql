66867fe36e46066089a8b45082e8140d3e9b3fdc
Stepan <stepan@MBP-Stepan.Dlink>
2023-07-23 19:52:31 +0300

    all good

--- migrations/000006_change_product_table.down.sql
+++ migrations/000006_change_product_table.down.sql
@@ -1,2 +1,1 @@
 ALTER TABLE product DROP FOREIGN KEY fk_category_id;
-ALTER TABLE product DROP COLUMN category_id;
--- migrations/000006_change_product_table.up.sql
+++ migrations/000006_change_product_table.up.sql
@@ -1,5 +1,4 @@
 ALTER TABLE product
-ADD category_id int NULL,
 ADD CONSTRAINT fk_category_id
 FOREIGN KEY(category_id) REFERENCES category(category_id)
         on delete cascade
